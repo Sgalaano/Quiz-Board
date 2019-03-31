0b507c7339af9faa0fa5cfef2ff92c8917a08349
sgalaano <sgalaano@gmail.com>
2019-03-31 10:55:55 +0300

    cheanged some values

--- Js/script.js
+++ Js/script.js
@@ -2,19 +2,19 @@
 
 $(document).ready(function() {
   $("#blankForm").submit(function(event) {
+    event.preventDefault();
     var quiz1 = $("q1").val();
     var quiz2 = $("q2").val();
     var quiz3 = $("q3").val();
     var quiz4 = $("q4").val();
     var quiz5 = $("q5").val()
 
-    var resultScore = parseInt(q1) + parseInt(q2) + parseInt(q3) + parseInt(q4) + parseInt(q5)
-    $("#finish").text("You SCored: " + resultScore + " out of 100");
-
-
-    //==== User Interface ====//
-    $("#finish").show();
-    event.preventDefault();
+    var score = parseInt(q1+q2+q3+q4+q5);
+    if (score>70){
+     $("#input").text("Your score is: " + score + "you have passed");
+    }
+    else {
+      $("#input").text("You failed please retake exam");
+    }
   });
-
 });
