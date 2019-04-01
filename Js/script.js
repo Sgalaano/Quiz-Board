470015721e9509d7b9ad10a4394bbf516364e5eb
sgalaano <sgalaano@gmail.com>
2019-04-01 14:17:13 +0300

    Modified script.js

--- Js/script.js
+++ Js/script.js
@@ -1,20 +1,32 @@
-//==== Business logic====//
+function check() {
+  var q1 = document.Quiz.q1.value;
+  var q2 = document.Quiz.q2.value;
+  var q3 = document.Quiz.q3.value;
+  var q4 = document.Quiz.q4.value;
+  var q5 = document.Quiz.q5.value;
+  var right = 0;
 
-$(document).ready(function() {
-  $("#blankForm").submit(function(event) {
-    event.preventDefault();
-    var quiz1 = $("q1").val();
-    var quiz2 = $("q2").val();
-    var quiz3 = $("q3").val();
-    var quiz4 = $("q4").val();
-    var quiz5 = $("q5").val()
+// user interface
+  if (q1 == "correct") {
+      right = right + 20;
+  };
+  if (q2 == "correct") {
+      right = right + 20;
+  };
+  if (q3 == "correct") {
+      right = right + 20;
+  };
+  if (q4 == "correct") {
+      right = right + 20;
+  };
+  if (q5 == "correct") {
+      right = right + 20;
+  };
 
-    var score = parseInt(q1+q2+q3+q4+q5);
-    if (score>70){
-     $("#input").text("Your score is: " + score + "you have passed");
-    }
-    else {
-      $("#input").text("You failed please retake exam");
-    }
-  });
-});
+
+
+  document.getElementById("fifth-p").style.visibility = "visible";
+  document.getElementById("correctNumber").innerHTML = "HERE IS YOUR SCORE: " + right + "%";
+  $(".container").fadeOut();
+
+};
